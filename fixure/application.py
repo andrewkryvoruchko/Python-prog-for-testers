89d915c8db66964fe6273bc44b458449a99d7074
andrewkryvoruchko <andrew.kryvoruchko@gmail.com>
2019-02-21 22:18:12 +0200

    decomposition выделен класс для управления сессиями

--- fixure/application.py
+++ fixure/application.py
@@ -1,47 +1,45 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from fixure.session import SessionHelper
 
 class Application:
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self) # помошнику передается объект класса Application
 
     def distroy(self):
-        self.driver.quit()
+        wd = self.wd
+        wd.quit()
 
     def open_home_page(self):
-        self.driver.get("http://prestashop.qatestlab.com.ua/ru/")
-
-    def login(self, username, password):
-        self.open_home_page()
-        self.driver.find_element_by_css_selector(".header_user_info a").click()
-        self.driver.find_element_by_id("email").send_keys(username)
-        self.driver.find_element_by_id("passwd").send_keys(password)
-        self.driver.find_element_by_name("SubmitLogin").click()
+        wd = self.wd
+        wd.get("http://prestashop.qatestlab.com.ua/ru/")
 
     def information(self, address):
-        self.driver.find_element_by_id("firstname").send_keys(address.first_name)
-        self.driver.find_element_by_id("lastname").send_keys(address.last_name)
-        self.driver.find_element_by_id("company").send_keys(address.company)
-        self.driver.find_element_by_id("address1").send_keys(address.address)
-        self.driver.find_element_by_id("postcode").send_keys(address.zipcode)
-        self.driver.find_element_by_id("city").send_keys(address.city)
-        self.driver.find_element_by_id("id_country").send_keys(address.country)
-        self.driver.find_element_by_id("phone").send_keys(address.home_phone)
-        self.driver.find_element_by_id("phone_mobile").send_keys(address.mobile_phone)
-        self.driver.find_element_by_id("id_state").send_keys(address.state)
-        self.driver.find_element_by_id("alias").send_keys(address.assign)
-        self.driver.find_element_by_id("submitAddress").click()
+        wd = self.wd
+        wd.find_element_by_id("firstname").send_keys(address.first_name)
+        wd.find_element_by_id("lastname").send_keys(address.last_name)
+        wd.find_element_by_id("company").send_keys(address.company)
+        wd.find_element_by_id("address1").send_keys(address.address)
+        wd.find_element_by_id("postcode").send_keys(address.zipcode)
+        wd.find_element_by_id("city").send_keys(address.city)
+        wd.find_element_by_id("id_country").send_keys(address.country)
+        wd.find_element_by_id("phone").send_keys(address.home_phone)
+        wd.find_element_by_id("phone_mobile").send_keys(address.mobile_phone)
+        wd.find_element_by_id("id_state").send_keys(address.state)
+        wd.find_element_by_id("alias").send_keys(address.assign)
+        wd.find_element_by_id("submitAddress").click()
 
     def add_address(self, address):
-        self.driver.find_element_by_xpath("//div[@id='center_column']//a[@title='Add my first address']").click()
+        wd = self.wd
+        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Add my first address']").click()
         self.information(address)
 
     def add_new_address(self, address):
-        self.driver.find_element_by_xpath("//div[@id='center_column']//a[@title='Addresses']").click()
-        self.driver.find_element_by_xpath("//div[@id='center_column']//a[@title='Add an address']").click()
+        wd = self.wd
+        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Addresses']").click()
+        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Add an address']").click()
         self.information(address)
 
-    def logout(self):
-        self.driver.find_element_by_xpath("//div[@class='row']//a[text()='Выйти']").click()
--- test/test_add_address.py
+++ test/test_add_address.py
@@ -10,16 +10,16 @@ def app(request):
     return fixture
 
 def test_add_first_address(app):
-    app.login(username="andrew1973@gmail.com", password="giovanni")
+    app.session.login(username="andrew1973@gmail.com", password="giovanni")
     app.add_address(Address(first_name="andrew", last_name="kriv", company="IT", address="Ivana Franka 5/16",
                          zipcode="12345", city="Kyiv", country="Украина", home_phone="123456",
                          mobile_phone="123456", state="Киевская область", assign="andr"))
-    app.logout()
+    app.session.logout()
 
 def test_add_second_address(app):
-    app.login(username="andrew1973@gmail.com", password="giovanni")
+    app.session.login(username="andrew1973@gmail.com", password="giovanni")
     app.add_new_address(Address(first_name="andrew", last_name="kriv", company="IT", address="Lvivska Ploshia 7/20",
                              zipcode="67890", city="Kyiv", country="Ukrane", home_phone="789123",
                              mobile_phone="789123", state="Киевская область", assign="andr"))
-    app.logout()
+    app.session.logout()
 
