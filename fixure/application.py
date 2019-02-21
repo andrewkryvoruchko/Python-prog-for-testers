f91c21cd1d2bc834d81b49c596b2c1a54abb740c
andrewkryvoruchko <andrew.kryvoruchko@gmail.com>
2019-02-21 23:30:01 +0200

    decomposition выделен класс для работы с группами

--- fixure/application.py
+++ fixure/application.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from fixure.session import SessionHelper
+from fixure.address import AddressHelper
 
 class Application:
 
@@ -8,6 +9,7 @@ class Application:
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self) # помошнику передается объект класса Application
+        self.address = AddressHelper(self) #
 
     def distroy(self):
         wd = self.wd
@@ -16,30 +18,4 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://prestashop.qatestlab.com.ua/ru/")
-
-    def information(self, address):
-        wd = self.wd
-        wd.find_element_by_id("firstname").send_keys(address.first_name)
-        wd.find_element_by_id("lastname").send_keys(address.last_name)
-        wd.find_element_by_id("company").send_keys(address.company)
-        wd.find_element_by_id("address1").send_keys(address.address)
-        wd.find_element_by_id("postcode").send_keys(address.zipcode)
-        wd.find_element_by_id("city").send_keys(address.city)
-        wd.find_element_by_id("id_country").send_keys(address.country)
-        wd.find_element_by_id("phone").send_keys(address.home_phone)
-        wd.find_element_by_id("phone_mobile").send_keys(address.mobile_phone)
-        wd.find_element_by_id("id_state").send_keys(address.state)
-        wd.find_element_by_id("alias").send_keys(address.assign)
-        wd.find_element_by_id("submitAddress").click()
-
-    def add_address(self, address):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Add my first address']").click()
-        self.information(address)
-
-    def add_new_address(self, address):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Addresses']").click()
-        wd.find_element_by_xpath("//div[@id='center_column']//a[@title='Add an address']").click()
-        self.information(address)
 
--- test/test_add_address.py
+++ test/test_add_address.py
@@ -11,15 +11,15 @@ def app(request):
 
 def test_add_first_address(app):
     app.session.login(username="andrew1973@gmail.com", password="giovanni")
-    app.add_address(Address(first_name="andrew", last_name="kriv", company="IT", address="Ivana Franka 5/16",
-                         zipcode="12345", city="Kyiv", country="Украина", home_phone="123456",
-                         mobile_phone="123456", state="Киевская область", assign="andr"))
+    app.address.add(Address(first_name="andrew", last_name="kriv", company="IT", address="Ivana Franka 5/16",
+                            zipcode="12345", city="Kyiv", country="Украина", home_phone="123456",
+                            mobile_phone="123456", state="Киевская область", assign="andr"))
     app.session.logout()
 
 def test_add_second_address(app):
     app.session.login(username="andrew1973@gmail.com", password="giovanni")
-    app.add_new_address(Address(first_name="andrew", last_name="kriv", company="IT", address="Lvivska Ploshia 7/20",
-                             zipcode="67890", city="Kyiv", country="Ukrane", home_phone="789123",
-                             mobile_phone="789123", state="Киевская область", assign="andr"))
+    app.address.add_new(Address(first_name="andrew", last_name="kriv", company="IT", address="Lvivska Ploshia 7/20",
+                                zipcode="67890", city="Kyiv", country="Ukrane", home_phone="789123",
+                                mobile_phone="789123", state="Киевская область", assign="andr"))
     app.session.logout()
 
